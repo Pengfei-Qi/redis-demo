fcc1448fb2947e5002d53521b7f71279ac9d7afc
pengfei.qi <847469604@qq.com>
2023-08-30 15:26:50 +0800

    解决状态登录刷新的问题

--- src/main/java/com/union/redisdemo/config/WebMvcConfig.java
+++ src/main/java/com/union/redisdemo/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.union.redisdemo.config;
 
 import com.union.redisdemo.utils.LoginInterceptor;
+import com.union.redisdemo.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,12 +17,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate)).excludePathPatterns(
+        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns(
                 "/user/code",
                 "/user/login",
                 "/shop-type/**",
                 "/voucher/**",
-                "/upload/**"
-        );
+                "/upload/**",
+                "/blog/hot",
+                "/shop/**"
+        ).order(1);
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
     }
 }
--- src/main/java/com/union/redisdemo/utils/LoginInterceptor.java
+++ src/main/java/com/union/redisdemo/utils/LoginInterceptor.java
@@ -1,62 +1,22 @@
 package com.union.redisdemo.utils;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.StrUtil;
-import com.union.redisdemo.dto.UserDTO;
-import com.union.redisdemo.entity.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import static com.union.redisdemo.utils.RedisConstants.LOGIN_USER_TTL;
 
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
-    private StringRedisTemplate stringRedisTemplate;
-
-    public StringRedisTemplate getStringRedisTemplate() {
-        return stringRedisTemplate;
-    }
-
-    public void setStringRedisTemplate(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-
-    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         log.info("url:{}",request.getRequestURL().toString());
-        String token = request.getHeader("authorization");
-        if (StrUtil.isBlank(token)){
+        if (UserHolder.getUser() == null){
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return false;
         }
-        String tokenKey = RedisConstants.LOGIN_USER_KEY + token;
-        Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries(tokenKey);
-        UserDTO user = BeanUtil.fillBeanWithMap(entries, new UserDTO(), true);
-        if (ObjectUtil.isEmpty(user)){
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            return false;
-        }
-        UserHolder.saveUser(user);
-        stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL, TimeUnit.SECONDS);//一个小时
         return true;
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
     }
 }
