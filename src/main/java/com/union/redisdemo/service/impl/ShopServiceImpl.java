89291fa1312bc5363e986a4c932f385a81182bfe
pengfei.qi <847469604@qq.com>
2023-09-01 14:34:36 +0800

    使用互斥锁，解决缓存击穿问题

--- src/main/java/com/union/redisdemo/service/impl/ShopServiceImpl.java
+++ src/main/java/com/union/redisdemo/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.union.redisdemo.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,7 +26,10 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Override
     public Result queryById(Long id) {
         //解决缓存穿透问题
-        Shop shop = getShopWithCacheThrough(id);
+        // Shop shop = getShopWithCacheThrough(id);
+
+        //缓存重建、使用互斥锁，解决缓存击穿
+        Shop shop = getShopWithCacheHit(id);
 
         if (shop == null){
             return Result.fail("商品不存在");
@@ -62,6 +66,47 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return shop;
 
+    }
+
+    public Shop getShopWithCacheHit(Long id){
+        String key = RedisConstants.CACHE_SHOP_KEY +id;
+
+        String shopStr = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isNotBlank(shopStr)){
+            return JSONUtil.toBean(shopStr, Shop.class);
+        }
+        if (shopStr != null){
+            return null;
+        }
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        try {
+            TimeUnit.SECONDS.sleep(1);
+            boolean isLock = lockShop(lockKey);
+            if (!isLock){
+                TimeUnit.MILLISECONDS.sleep(10);
+                return getShopWithCacheHit(id);
+            }
+            Shop shop = getById(id);
+            if (shop == null){
+                stringRedisTemplate.opsForValue().set(key, "",RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return null;
+            }
+            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+            return shop;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            unlockShop(lockKey);
+        }
+    }
+
+    private boolean lockShop(String key){
+        Boolean aBoolean = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", RedisConstants.LOCK_SHOP_TTL, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(aBoolean);
+    }
+
+    private void unlockShop(String key){
+        stringRedisTemplate.delete(key);
     }
 
     @Override
